c6e5256634f8cda21aedc3d289f14a49f07b561d
AbhishekDesai99 <abhishekdesai6210@gmail.com>
2025-01-12 20:32:08 +0530

    removed empty files

--- src/user/controllers/update-profile/index.js
+++
@@ -1,3 +0,0 @@
-export const updateProfileHandler = async (event) => {
-    
-}
--- src/validators/user/index.js
+++
@@ -1,8 +0,0 @@
-import 'source-map-support/register';
-import Joi from 'joi';
-
-export const joiUserSchema = Joi.object({
-    name: Joi.string().required(),
-    age: Joi.number().integer().min(18).required(),
-    email: Joi.string().email().required(),
-});
